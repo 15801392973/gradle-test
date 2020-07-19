860168232600352af20fd8998ae226f167af6ed8
fanshuai <fanshuai@xueqiu.com>
2020-07-19 20:26:53 +0800

    gradle搭建项目demo

---
+++ common/src/main/java/com/fanshuai/Main.java
@@ -0,0 +1,7 @@
+package com.fanshuai;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ web/src/main/java/com/fanshuai/FannshuaiApp.java
@@ -0,0 +1,14 @@
+package com.fanshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class FannshuaiApp {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(FannshuaiApp.class, args);
+
+
+    }
+}
